78e699d54d3520c5b1f63b8fc07328b61ba96fd5
jmgpena <jmgpena@gmail.com>
2024-06-24 13:45:08 +0100

    Add neovim config and clean up bash

---
+++ private_dot_config/nvim/init.lua
@@ -0,0 +1,360 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Example using a list of specs with the default options
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.fileformats = "unix,dos"
+
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
+
+vim.opt.conceallevel = 2
+vim.opt.guifont = "Cascadia Mono NF:h11"
+
+require("lazy").setup({
+  "folke/which-key.nvim",
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  "nvim-tree/nvim-web-devicons",
+  "nvim-lualine/lualine.nvim",
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.6",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+  },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+  { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
+  { -- optional completion source for require statements and module annotations
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, {
+        name = "lazydev",
+        group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+      })
+    end,
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    version = false, -- last release is way too old and doesn't work on Windows
+    build = ":TSUpdate",
+    config = function()
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+        ensure_installed = {
+          "c",
+          "lua",
+          "vim",
+          "vimdoc",
+          "query",
+          "python",
+          "bash",
+          "javascript",
+          "typescript",
+          "html",
+          "make",
+          "php",
+          "zig",
+          "markdown",
+          "markdown_inline",
+        },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
+  "mbbill/undotree",
+  --- Uncomment the two plugins below if you want to manage the language servers from neovim
+  { "williamboman/mason.nvim" },
+  { "williamboman/mason-lspconfig.nvim" },
+  { "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
+  { "neovim/nvim-lspconfig" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/nvim-cmp" },
+  { "L3MON4D3/LuaSnip" },
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+    --   "BufReadPre path/to/my-vault/**.md",
+    --   "BufNewFile path/to/my-vault/**.md",
+    -- },
+
+    keys = {
+      { "<leader>on", "<cmd>ObsidianNew<cr>", desc = "New Obsidian note", mode = "n" },
+      { "<leader>oo", "<cmd>ObsidianSearch<cr>", desc = "Search Obsidian notes", mode = "n" },
+      { "<leader>os", "<cmd>ObsidianQuickSwitch<cr>", desc = "Quick Switch", mode = "n" },
+      { "<leader>ob", "<cmd>ObsidianBacklinks<cr>", desc = "Show location list of backlinks", mode = "n" },
+      { "<leader>ot", "<cmd>ObsidianTemplate<cr>", desc = "Follow link under cursor", mode = "n" },
+      { "<leader>op", "<cmd>ObsidianPasteImg<cr>", desc = "Paste imate from clipboard under cursor", mode = "n" },
+      { "<leader>od", "<cmd>ObsidianToday<cr>", desc = "Go to today's daily note", mode = "n" },
+      { "<leader>ot", "<cmd>ObsidianTomorrow<cr>", desc = "Go to tomorrow's daily note", mode = "n" },
+    },
+
+    dependencies = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
+      -- see below for full list of optional dependencies 👇
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "personal",
+          path = "~/obsidian",
+        },
+      },
+
+      -- see below for full list of options 👇
+      daily_notes = {
+        -- Optional, if you keep daily notes in a separate directory.
+        folder = "calendar",
+        -- Optional, if you want to change the date format for the ID of daily notes.
+        date_format = "%Y-%m-%d",
+        -- Optional, if you want to change the date format of the default alias of daily notes.
+        alias_format = "%B %-d, %Y",
+        -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+        template = nil,
+      },
+
+      -- note naming
+      note_id_func = function(title)
+        return title
+      end,
+      disable_frontmatter = true,
+    },
+  },
+  {
+    "mikavilpas/yazi.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    event = "VeryLazy",
+    keys = {
+      -- 👇 in this section, choose your own keymappings!
+      {
+        "<leader>-",
+        function()
+          require("yazi").yazi()
+        end,
+        desc = "Open the file manager",
+      },
+      {
+        -- Open in the current working directory
+        "<leader>cw",
+        function()
+          require("yazi").yazi(nil, vim.fn.getcwd())
+        end,
+        desc = "Open the file manager in nvim's working directory",
+      },
+    },
+    ---@type YaziConfig
+    opts = {
+      open_for_directories = false,
+    },
+  },
+  --- neorg
+  -- {
+  --   "vhyrro/luarocks.nvim",
+  --   priority = 1000, -- We'd like this plugin to load first out of the rest
+  --   config = true, -- This automatically runs `require("luarocks-nvim").setup()`
+  -- },
+  -- {
+  --   "nvim-neorg/neorg",
+  --   dependencies = { "luarocks.nvim" },
+  --   -- put any other flags you wanted to pass to lazy here!
+  --   lazy = false,
+  --   version = "*",
+  --   config = true,
+  -- }
+  {
+    "nvim-orgmode/orgmode",
+    event = "VeryLazy",
+    ft = { "org" },
+    config = function()
+      -- Setup orgmode
+      require("orgmode").setup({
+        org_agenda_files = "~/.org/**/*",
+        org_default_notes_file = "~/.org/notes.org",
+      })
+
+      -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+      -- add ~org~ to ignore_install
+      -- require('nvim-treesitter.configs').setup({
+      --   ensure_installed = 'all',
+      --   ignore_install = { 'org' },
+      -- })
+    end,
+  },
+})
+
+-- Apply theme
+vim.cmd([[colorscheme tokyonight]])
+require("lualine").setup()
+
+-- LSP Zero
+local lsp_zero = require("lsp-zero")
+
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({ buffer = bufnr })
+end)
+lsp_zero.setup()
+
+-- to learn how to use mason.nvim
+-- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
+require("mason").setup({})
+require("mason-lspconfig").setup({
+  ensure_installed = {},
+  handlers = {
+    function(server_name)
+      require("lspconfig")[server_name].setup({})
+    end,
+  },
+})
+
+-- Telescope bindings
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+vim.keymap.set("n", "<leader>fe", vim.cmd.Ex) -- File explorer
+vim.keymap.set("n", "<leader>fs", vim.cmd.update)
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeShow)
+vim.keymap.set("n", "<leader>x", '"_dP')
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+Y')
+vim.keymap.set("n", "<leader>p", '"+p')
+
+vim.keymap.set("n", "<leader>`", "<C-^>")
+
+vim.keymap.set("n", "<leader>d", '"_d')
+vim.keymap.set("v", "<leader>d", '"_d')
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- GPG Auto decryption/encryption
+local gpgGroup = vim.api.nvim_create_augroup("customGpg", { clear = true })
+
+-- autocmds execute in the order in which they were defined.
+-- https://neovim.io/doc/user/autocmd.html#autocmd-define
+
+vim.api.nvim_create_autocmd({ "BufReadPre", "FileReadPre" }, {
+  pattern = "*.gpg",
+  group = gpgGroup,
+  callback = function()
+    -- Make sure nothing is written to shada file while editing an encrypted file.
+    vim.opt_local.shada = nil
+    -- We don't want a swap file, as it writes unencrypted data to disk
+    vim.opt_local.swapfile = false
+    -- Switch to binary mode to read the encrypted file
+    vim.opt_local.bin = true
+
+    vim.cmd("let ch_save = &ch|set ch=2")
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufReadPost", "FileReadPost" }, {
+  pattern = "*.gpg",
+  group = gpgGroup,
+  callback = function()
+    vim.cmd("'[,']!gpg --decrypt -q 2> /dev/null")
+
+    -- Switch to normal mode for editing
+    vim.opt_local.bin = false
+
+    vim.cmd("let &ch = ch_save|unlet ch_save")
+    vim.cmd(":doautocmd BufReadPost " .. vim.fn.expand("%:r"))
+  end,
+})
+
+-- Convert all text to encrypted text before writing
+vim.api.nvim_create_autocmd({ "BufWritePre", "FileWritePre" }, {
+  pattern = "*.gpg",
+  group = gpgGroup,
+  command = "'[,']!gpg --default-recipient-self -ae 2>/dev/null",
+})
+-- Undo the encryption so we are back in the normal text, directly
+-- after the file has been written.
+vim.api.nvim_create_autocmd({ "BufWritePost", "FileWritePost" }, {
+  pattern = "*.gpg",
+  group = gpgGroup,
+  command = "u",
+})
