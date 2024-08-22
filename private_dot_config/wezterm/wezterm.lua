9bda58d56b38a766a6ecb6a754453d132487e65c
jmgpena <jmgpena@gmail.com>
2024-08-22 15:37:11 +0100

    Update tabs and window decorations

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -5,11 +5,13 @@ local config = {}
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
+config.automatically_reload_config = true
 
 -- appearence
 config.color_scheme = "Tokyo Night"
 config.font = wezterm.font("Cascadia Mono NF")
 config.font_size = 11.0
+config.window_decorations = "RESIZE"
 
 -- start shell for different systems
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
@@ -99,7 +101,7 @@ config.keys = {
 }
 
 -- <leader> 0-9 to switch tabs
-for i = 1, 9 do
+for i = 0, 9 do
 	-- leader + number to activate that tab
 	table.insert(config.keys, {
 		key = tostring(i),
@@ -109,9 +111,66 @@ for i = 1, 9 do
 end
 
 -- tab bar
+config.tab_and_split_indices_are_zero_based = true
 config.hide_tab_bar_if_only_one_tab = false
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
+
+-- The filled in variant of the < symbol
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
+
+-- The filled in variant of the > symbol
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
+
+-- This function returns the suggested title for a tab.
+-- It prefers the title that was set via `tab:set_title()`
+-- or `wezterm cli set-tab-title`, but falls back to the
+-- title of the active pane in that tab.
+function tab_title(tab_info)
+	local title = tab_info.tab_title
+	local index = tab_info.tab_index
+	-- if the tab title is explicitly set, take that
+	if title and #title > 0 then
+		return tostring(index) .. ":" .. title
+	end
+	-- Otherwise, use the title from the active pane
+	-- in that tab
+	return tostring(index) .. ":" .. tab_info.active_pane.title
+end
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local edge_background = "#0b0022"
+	local background = "#1b1032"
+	local foreground = "#808080"
+
+	if tab.is_active then
+		background = "#2b2042"
+		foreground = "#c0c0c0"
+	elseif hover then
+		background = "#3b3052"
+		foreground = "#909090"
+	end
+
+	local edge_foreground = background
+
+	local title = tab_title(tab)
+
+	-- ensure that the titles fit in the available space,
+	-- and that we have room for the edges.
+	title = wezterm.truncate_right(title, max_width - 2)
+
+	return {
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_LEFT_ARROW },
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Text = title },
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_RIGHT_ARROW },
+	}
+end)
 
 -- tmux status
 wezterm.on("update-right-status", function(window, _)
